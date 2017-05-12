17ca9281236dba06d906dd7ccc80f9dcd089d200
bruce84 <lixinyang.kenya@gmail.com>
2017-05-12 16:56:26 +0800

    2017修改
    2017修改java文件

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld{
   public static void main(String[] args){
     System.out.println("Hello World!");
     System.out.println("Some edits");
+    System.out.println("changes in 2017");
   }
 }  
